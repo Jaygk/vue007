c03f8c65f9a29e3f1cdff24e5399288fc9ec7771
Jaygk <957569035@qq.com>
2019-08-08 15:16:20 +0800

    init my project

--- src/main.js
+++ src/main.js
@@ -1,2 +1,17 @@
 // 这是项目js的入口文件
-console.log('ok');
+// console.log('ok');
+import Vue from 'vue';
+
+// 导入 MUI 的样式
+import './lib/mui/css/mui.min.css';
+
+// 按需导入 mint-ui 中的组件
+import { Header } from 'mint-ui';
+Vue.component(Header.name, Header);
+
+import app from './app.vue';
+
+const vm = new Vue({
+    el: '#app',
+    render: c => c(app)
+})
